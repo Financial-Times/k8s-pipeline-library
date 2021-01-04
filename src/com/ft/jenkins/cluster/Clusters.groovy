d07aa10f4250f6204f309aed10a0928a79238f84
Mihail Mihaylov <mihail.mihaylov@ft.com>
2021-01-04 15:16:19 +0200

    New API endpoint for eks-publish-dev-eu

--- src/com/ft/jenkins/cluster/Clusters.groovy
+++ src/com/ft/jenkins/cluster/Clusters.groovy
@@ -110,7 +110,7 @@ class Clusters implements Serializable {
       clusterToApiServerMap = [
               ("${Region.EU}-${ClusterType.PUBLISHING}".toString()): newEksEntry(
                       eksClusterName: "eks-publish-dev-eu",
-                      apiServer: "https://EFEB69D31794C7043F17AD8187FCE71F.gr7.eu-west-1.eks.amazonaws.com",
+                      apiServer: "https://25C9DAC5C2EA30A99DE9DBE542EF6EBC.gr7.eu-west-1.eks.amazonaws.com",
                       publicEndpoint: "https://upp-k8s-dev-publish-eu.upp.ft.com"
               )
       ]
@@ -128,7 +128,7 @@ class Clusters implements Serializable {
       clusterToApiServerMap = [
               ("${Region.EU}-${ClusterType.PUBLISHING}".toString()): newEksEntry(
                       eksClusterName: "eks-publish-dev-eu",
-                      apiServer: "https://EFEB69D31794C7043F17AD8187FCE71F.gr7.eu-west-1.eks.amazonaws.com",
+                      apiServer: "https://25C9DAC5C2EA30A99DE9DBE542EF6EBC.gr7.eu-west-1.eks.amazonaws.com",
                       publicEndpoint: "https://upp-k8s-dev-publish-eu.upp.ft.com"
               )
       ]
